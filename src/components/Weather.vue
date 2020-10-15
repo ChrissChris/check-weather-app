bf8c2cfd8a964ce5094d7ccdfc710d1c9bbeed76
Chriss Chris <zenskech@outlook.com>
2020-10-15 13:33:37 -0400

    Update Weather.vue

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -72,7 +72,7 @@ export default {
           );
         })
         .catch(() => {
-          console.log("Please come back later, thank you  :)");
+          console.log("Please come back later, thank you :)");
         });
     },
   },
