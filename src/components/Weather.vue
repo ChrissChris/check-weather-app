1245217ff33d9b4e1a86e49b681120b2b82b3147
Chriss Chris <zenskech@outlook.com>
2020-10-23 11:43:29 -0400

    Update Weather.vue

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -9,7 +9,7 @@
   >
     <v-card-text id="temp">
       <h2 id="today" class="">Today Weather</h2>
-      <div v-if="kalvinTemperatureToday" id="text-wraper">
+      <div v-if="kalvinTemperatureToday" id="temperature-text-wraper">
         <h1 id="celziusTemperatureDisplay">{{ celciusTemperature }} °C</h1>
 
         <p id="niceDayWishing">Have a nice day</p>
